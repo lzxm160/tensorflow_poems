1c2f273c5ee1387bf69c7e68f413f52fb31990d1
lzxm160 <lzxm160@sina.com>
2020-02-04 09:52:06 +0800

    x

--- compose_poem.py
+++ compose_poem.py
@@ -91,7 +91,7 @@ def pretty_print_poem(poem_):
 
 if __name__ == '__main__':
     # begin_char = input('## please input the first character:')
-    begin_word = '明'
+    begin_word = '雨'
     print(begin_word)
     poem = gen_poem(begin_word)
     pretty_print_poem(poem_=poem)
